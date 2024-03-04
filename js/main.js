f4e9ea4b5ef78d02b5bab0e7944ec98781f141c9
dudova-lisa <lizadudova91@gmail.com>
2024-03-04 16:02:40 +0200

    fix: navbar height scroll

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ const logo = document.querySelector(".logo-svg use");
 window.addEventListener("scroll", () => {
   if (this.scrollY > 1) {
     navbar.classList.add("navbar-light");
-    logo.href.beseVal = "img/sprite.svg#logo";
+    logo.href.baseVal = "img/sprite.svg#logo";
   } else {
     navbar.classList.remove("navbar-light");
-    logo.href.beseVal = "img/sprite.svg#logo-light";
+    logo.href.baseVal = "img/sprite.svg#logo-light";
   }
 });
