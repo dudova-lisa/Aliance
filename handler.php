cb017c2a9e4c82a7fc76549256e23f43640c9354
dudova-lisa <lizadudova91@gmail.com>
2024-04-02 17:52:20 +0200

    fix: tgBot and navbar hover

--- handler.php
+++ handler.php
@@ -14,11 +14,11 @@ foreach($formData as $key => $value) {
   $text .= $key . "<b>" . urlencode($value) . "</b>" . "%0A";
 }
 
-$sendToTelegram = fopen("https://api.telegram.org/bot{$tocken}/sendMassage?chat_id={$chat_id}&text={$text}&parse_mode=html", "r");
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text={$text}&parse_mode=html", "r");
 
 if ($sendToTelegram) {
   echo "Success";
 } else {
-  echo "Error"
+  echo "Error";
 }
 
