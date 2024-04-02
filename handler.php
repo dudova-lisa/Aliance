eae399bc089008de3015c4537abd04991e18f457
dudova-lisa <lizadudova91@gmail.com>
2024-04-02 14:11:48 +0200

    add: tg-Bot

---
+++ handler.php
@@ -0,0 +1,24 @@
+<?php
+$user_name = htmlspecialchars($_POST["username"]);
+$user_phone = htmlspecialchars($_POST["userphone"]);
+
+$token = "7090081960:AAHdQreoDhsftYjAbOhM6FjXAn5VRASMPuQ";
+$chat_id = "-4102014428";
+
+$formData = array(
+  "Клиент: " => $user_name, 
+  "Телефон: " => $user_phone
+);
+
+foreach($formData as $key => $value) {
+  $text .= $key . "<b>" . urlencode($value) . "</b>" . "%0A";
+}
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$tocken}/sendMassage?chat_id={$chat_id}&text={$text}&parse_mode=html", "r");
+
+if ($sendToTelegram) {
+  echo "Success";
+} else {
+  echo "Error"
+}
+
